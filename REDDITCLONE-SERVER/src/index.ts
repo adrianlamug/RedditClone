dab163175485cc84e81a329204bcc136bd2106ec
adrianlamug <adrianlamug7@gmail.com>
2022-09-29 00:01:01 +0100

    set up apollo server express

--- REDDITCLONE-SERVER/src/index.ts
+++ REDDITCLONE-SERVER/src/index.ts
@@ -1,7 +1,11 @@
 import { MikroORM} from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
-import mikroConfig from "./mikro-orm.config"
+import mikroConfig from "./mikro-orm.config";
+import express from 'express';
+import {ApolloServer} from 'apollo-server-express';
+import {buildSchema} from 'type-graphql';
+import { HelloResolver } from "./resolvers/hello";
 
 
 const main = async() => {
@@ -13,8 +17,24 @@ const main = async() => {
     // const post = orm.em.create(Post, {title: 'my first post'});
     // what to insert into the database
     // await orm.em.persistAndFlush(post);
-    const posts = await orm.em.find(Post, {});
-    console.log(posts);
+    // const posts = await orm.em.find(Post, {}); 
+    // console.log(posts); 
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        }),
+    });
+
+    await apolloServer.start();
+    // create graphql endpoint for us on express
+    apolloServer.applyMiddleware({app});
+
+    app.listen(4000, () => {
+        console.log('server started on localhost:4000')
+    })
 
 }
 
